9b4399ecef39d35d98e7997b233d7a23d6bacd60
nounouhr2020 <113707240+nounouhr2020@users.noreply.github.com>
2026-03-30 23:03:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function scrollToContact() {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+}
+
+function callNow() {
+    alert("Appelez le numéro affiché pour nous contacter !");
+}
